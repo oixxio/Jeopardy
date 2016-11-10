ef782e2093118c60eb01562214c2e93d911e24d0
facundo ompre <facuompre@gmail.com>
2016-11-10 09:42:18 -0300

    first commit

---
+++ Assets/Scripts/GameSet.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSet : MonoBehaviour {
+
+    public GameObject card;
+	// Use this for initialization
+	void Start () {
+        GameObject card1 = (GameObject)Instantiate(card, new Vector3(0,0,0),Quaternion.identity);
+        card1.name = "card-1";
+        Instantiate(card, new Vector3(-2, 0, 0), Quaternion.identity);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            Physics.Raycast(ray, out hit);
+            Debug.Log(hit.transform.gameObject.name);
+            StartCoroutine(rotateCard(hit.transform.parent.gameObject));
+        }
+    }
+    private IEnumerator rotateCard(GameObject go)
+    {
+        float seconds = 0.005f;
+        for (int i = 1; i <= 45; i++)
+        {
+            yield return new WaitForSeconds(seconds);
+            go.transform.Rotate(Vector3.up * 4.0f);
+        }
+    }
+}
